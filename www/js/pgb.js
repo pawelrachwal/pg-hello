9c6e44f39baf342ac347951996cc38e663e7fc19
Paweł Rachwał <Pawel@Air-Pawe.home>
2017-03-26 18:25:21 +0200

    my business card

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -5,31 +5,3 @@ function init() {
 function onDeviceReady() {
 	navigator.notification.beep(1);
 }
-
-function deviceInfo() {
-
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
-
-	navigator.notification.alert(info);
-	
-}
-
-function myselfInfo() {
-
-	infoPersonal =  'Hi! My personal data:' + '\n' +
-			'=====' + '\n' +
-			'Paweł Rachwał' + '\n' + 
-			'Informatyka Stosowana' + '\n' + 
-			'KrDUIS1012' + '\n' + 
-			'181483' + '\n';
-
-	navigator.notification.alert(infoPersonal);
-	
-}
